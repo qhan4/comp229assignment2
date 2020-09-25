1388c6950b65965ecf0ee618b60791f2169fa495
Qiuli Han <qhan4@my.centennialcollege.ca>
2020-09-25 00:21:01 +0000

    All pages added, with some content populated.

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {status:err.status, message:err.message});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', page:'Home', menuId:'home' });
 });
 
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me', page:'About Me', menuId:'about' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects', page:'Projects', menuId:'projects'});
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services', page:'Services', menuId:'services'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me', page:'Contact Me', menuId:'contact'});
+});
+
+
 module.exports = router;
