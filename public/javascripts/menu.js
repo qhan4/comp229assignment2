7c319d09033983f9f6edca90222f3203d282817b
Qiuli Han <qhan4@my.centennialcollege.ca>
2020-09-29 22:38:01 +0000

    code clean up and comments

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,3 +1,10 @@
+/*
+style.css
+Qiuli Han
+301167000
+2020-09-29
+*/
+
 $(document).ready(function(){
     var element = $('meta[name="active-menu"]').attr('content');
     $('#' + element).addClass('active');
